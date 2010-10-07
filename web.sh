9f8a2629c2d2d8d1b195ddd428230f1789b20528
OJ Reeves <oj@buffered.io>
2010-10-08 00:45:02 +1000

    Added the csd_core dependency which does the Riak bits for us. Added a template to render snippets. Added a snippet resource to the webmachine site which handles rendering snippets. Fixed up the startup script to include csd_core Changed the haproxy config to just do the bits for webmachine. Added a dispatch to point to the new resource. Adjusted the .app file to include new modules.

--- web.sh
+++ web.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd `dirname $0`
-exec erl -pa $PWD/ebin $PWD/deps/*/ebin -boot start_sasl -s reloader -s csd
+exec erl -pa $PWD/ebin $PWD/deps/*/ebin -boot start_sasl -s reloader -s csd_core -s csd
